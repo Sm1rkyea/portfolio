ea873d9702da3776fb80e38185c2f714fee0417a
Smirkyea <smirkyea@gmail.com>
2025-07-10 18:27:23 +0200

    Added Images

--- constants/projects.ts
+++ constants/projects.ts
@@ -2,7 +2,7 @@ export const projectList = [
     {
         id: 1,
         date: '09.07.2025',
-        img: "/images/bg.png",
+        img: "/images/justdrven.png",
         project: 'JustDrven',
         label: 'Osobní portfolio',
         description: 'JustDrven je portfolio které bylo vytvořeno zcela zdarma pro jednoho z Developerů z Survival-Game.cz. Pracoval jsem na něm celkem 1 den v Figma, Next.js, TailwindCSS a ShadCN.',
@@ -14,7 +14,7 @@ export const influenceList = [
     {
         id: 1,
         date: '10.07.2025 - Present',
-        img: "/images/bg.png",
+        img: "/images/sg.png",
         project: 'Survival-Games.cz',
         role: 'Website Developer',
         label: 'Největší CZ/SK Minecraft projekt',
