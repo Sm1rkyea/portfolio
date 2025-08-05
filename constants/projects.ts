d15acf4a7757fc060fe3d3b36c4322c3a160c51d
Smirkyea <smirkyea@gmail.com>
2025-08-05 13:15:20 +0200

    Added project

--- constants/projects.ts
+++ constants/projects.ts
@@ -8,12 +8,21 @@ export const projectList = [
         description: 'JustDrven je portfolio které bylo vytvořeno zcela zdarma pro jednoho z Developerů na Survival-Games.cz. Pracoval jsem na něm celkem 1 den v Figma, Next.js, TailwindCSS a ShadCN.',
         link: 'https://drven.vercel.app',
     },
+    {
+        id: 1,
+        date: '05.08.2025',
+        img: "/images/ernite.png",
+        project: 'Ernite.fun',
+        label: 'Minecraftový server',
+        description: 'Ernite.fun je menší Minecraftový projekt u kterého jsem vytvořil Webovou stránku na zakázku. Na této stránce jsem pracoval přibližně 2 dny v Figma, Next.js, TailwindCSS a ShadCN.',
+        link: 'https://ernite.fun',
+    },
 ]
 
 export const influenceList = [
     {
         id: 1,
-        date: '10.07.2025 - Present',
+        date: '11.07.2025 - Present',
         img: "/images/sg.png",
         project: 'Survival-Games.cz',
         role: 'Website Developer',
